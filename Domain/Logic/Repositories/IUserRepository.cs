080cb290a81a523290748e15a77732271727db7e
MaximLebedevj <maximlebedevj@gmail.com>
2023-02-01 21:29:48 +0300

    added repositories

---
+++ Domain/Logic/Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Models;
+
+namespace Domain.Logic.Repositories;
+
+public interface IUserRepository : IRepository<User>
+{
+    Task<User?> GetByEmail(string phoneNumber);
+    Task<List<User>> GetAll();
+}

de5661f729997890923542072865882f0d95e7d8
MaximLebedevj <maximlebedevj@gmail.com>
2023-02-01 21:31:49 +0300

    added AppContext

---
+++ Database/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace Database;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get;set; }
+    public DbSet<Office> Offices { get; set; }
+    public DbSet<Country> Countries { get; set; }
+
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+}

3d6e38e3bef2cb27b9ec1512610c807a44f7d6ac
MaximLebedevj <maximlebedevj@gmail.com>
2023-02-01 21:32:20 +0300

    added AppContextFactory

---
+++ Database/ApplicationContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Database;
+
+public class ApplicationContextFactory : IDesignTimeDbContextFactory<ApplicationContext>
+{
+    public ApplicationContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+
+        string connectionString = "";
+
+        optionsBuilder.UseMySql(connectionString, new MariaDbServerVersion(new Version(10, 9, 4)));
+
+        return new ApplicationContext(optionsBuilder.Options);
+    }
+}

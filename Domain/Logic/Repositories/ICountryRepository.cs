8ff03203ccb3876a7de7a823b6398f29fa8e707d
MaximLebedevj <maximlebedevj@gmail.com>
2023-02-02 17:44:36 +0300

    added repositories

---
+++ Domain/Logic/Repositories/ICountryRepository.cs
@@ -0,0 +1,7 @@
+﻿using Domain.Models;
+
+namespace Domain.Logic.Repositories;
+public interface ICountryRepository : IRepository<Country>
+{
+    Task<List<Country>> GetAll();
+}
---
+++ Domain/Logic/Repositories/IOfficeRepository.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Models;
+
+namespace Domain.Logic.Repositories;
+
+public interface IOfficeRepository : IRepository<Office>
+{
+    Task<List<Office>> GetAll();
+}

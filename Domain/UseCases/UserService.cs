62db4892e5e1c389b3d5d7cb9d2db6bc3669bcb5
MaximLebedevj <maximlebedevj@gmail.com>
2023-02-02 16:17:01 +0300

    removed validation inside methods

--- Domain/UseCases/UserService.cs
+++ Domain/UseCases/UserService.cs
@@ -14,18 +14,13 @@ public class UserService
 
     public async Task<Result<User>> GetUser(int id)
     {
-        if (id <= 0)
-        {
-            return Result.Fail<User>("UserService.GetUser: Invalid id.");
-        }
-
         try
         {
             var success = await _db.Get(id);
 
             if (success is null)
             {
-                return Result.Fail<User>("UserService.GetUser: User doesn't exist.");
+                return Result.Fail<User>("User doesn't exist.");
             }
 
             return Result.Ok<User>(success);
@@ -39,18 +34,13 @@ public class UserService
 
     public async Task<Result<User>> Registration(User user)
     {
-        if (user.IsValid().IsFailure)
-        {
-            return Result.Fail<User>("UserService.Registration: " + user.IsValid().Error);
-        }
-
         try
         { 
             var item = await _db.GetByEmail(user.Email!);
 
             if (item is not null)
             {
-                return Result.Fail<User>("UserService.Registration: Email is already taken.");
+                return Result.Fail<User>("Email is already taken.");
             }
 
             user.Password = User.GeneratePassword(user.Password!);
@@ -67,30 +57,20 @@ public class UserService
 
     public async Task<Result<User>> Authorization(string email, string password)
     {
-        if (string.IsNullOrEmpty(email))
-        {
-            return Result.Fail<User>("UserService.Authorization: Null or empty Email.");
-        }
-
-        if (string.IsNullOrEmpty(password))
-        {
-            return Result.Fail<User>("UserService.Authorization: Null or empty Password.");
-        }
-
         try
         {
             var success = await _db.GetByEmail(email);
 
             if (success is null)
             {
-                return Result.Fail<User>("UserService.Authorization: User doesn't exist.");
+                return Result.Fail<User>("User doesn't exist.");
             }
 
             var generatedPassword = User.GeneratePassword(password);
 
             if (success.Password != generatedPassword)
             {
-                return Result.Fail<User>("UserService.Authorization: Wrong Password.");
+                return Result.Fail<User>("Wrong Password.");
             }
 
             return Result.Ok<User>(success);
@@ -103,11 +83,6 @@ public class UserService
 
     public async Task<Result<User>> UpdateUser(User user)
     {
-        if (user.IsValid().IsFailure)
-        {
-            return Result.Fail<User>("UserService.UpdateUser: " + user.IsValid().Error);
-        }
-
         try
         {
             var success = await _db.Update(user);
@@ -122,11 +97,6 @@ public class UserService
 
     public async Task<Result<User>> DeleteUser(int id)
     {
-        if (id <= 0)
-        {
-            return Result.Fail<User>("UserService.DeleteUser: Invalid id.");
-        }
-
         try
         {
             var success = await _db.Delete(id);
@@ -141,18 +111,13 @@ public class UserService
 
     public async Task<Result<User>> ActivateUser(int id)
     {
-        if (id <= 0)
-        {
-            return Result.Fail<User>("UserService.ActivateUser: Invalid id.");
-        }
-
         try
         {
             var success = await _db.Get(id);
 
             if (success is null)
             {
-                return Result.Fail<User>("UserService.ActivateUser: User doesn't exist.");
+                return Result.Fail<User>("User doesn't exist.");
             }
 
             success.Active = true;
@@ -169,11 +134,6 @@ public class UserService
 
     public async Task<Result<User>> DeactivateUser(int id)
     {
-        if (id <= 0)
-        {
-            return Result.Fail<User>("UserService.DeactivateUser: Invalid id.");
-        }
-
         try
         {
             var success = await _db.Get(id);
@@ -197,23 +157,13 @@ public class UserService
 
     public async Task<Result<User>> ChangeRole(int id, int roleId)
     {
-        if (id <= 0)
-        {
-            return Result.Fail<User>("UserService.ChangeRole: Invalid id.");
-        }
-
-        if (roleId <= 0)
-        {
-            return Result.Fail<User>("UserService.ChangeRole: Invalid roleId.");
-        }
-
         try
         {
             var success = await _db.Get(id);
 
             if (success is null)
             {
-                return Result.Fail<User>("UserService.ChangeRole: User doesn't exist.");
+                return Result.Fail<User>("User doesn't exist.");
             }
 
             success.RoleId= roleId;

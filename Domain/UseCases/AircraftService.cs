894415a6306dadf6f1ae7b17607f3530b27b50f8
MaximLebedevj <maximlebedevj@gmail.com>
2023-02-03 14:04:28 +0300

    added session2 base services

---
+++ Domain/UseCases/AircraftService.cs
@@ -0,0 +1,97 @@
+﻿using Domain.IRepositories;
+using Domain.Logic;
+using Domain.Models;
+
+namespace Domain.UseCases;
+public class AircraftService
+{
+    private IAircraftRepository _db;
+
+    public AircraftService(IAircraftRepository db)
+    {
+        _db = db;
+    }
+
+    public async Task<Result<Aircraft>> GetAircraft(int id)
+    {
+        try
+        {
+            var success = await _db.Get(id);
+
+            if (success is null)
+            {
+                return Result.Fail<Aircraft>("Aircraft doesn't exist.");
+            }
+
+            return Result.Ok<Aircraft>(success);
+        }
+        catch (Exception)
+        {
+            return Result.Exception<Aircraft>();
+        }
+    }
+
+    public async Task<Result<Aircraft>> DeleteAircraft(int id)
+    {
+        try
+        {
+            var success = await _db.Delete(id);
+
+            return Result.Ok<Aircraft>(success);
+        }
+        catch (Exception)
+        {
+            return Result.Exception<Aircraft>();
+        }
+    }
+
+    public async Task<Result<Aircraft>> UpdateAircraft(Aircraft aircraft)
+    {
+        try
+        {
+            var success = await _db.Update(aircraft);
+
+            return Result.Ok<Aircraft>(success);
+        }
+        catch (Exception)
+        {
+            return Result.Exception<Aircraft>();
+        }
+    }
+
+    public async Task<Result<Aircraft>> CreateAircraft(Aircraft aircraft)
+    {
+        try
+        {
+            var item = await _db.Get(aircraft.Id);
+
+            if (item is not null)
+            {
+                return Result.Fail<Aircraft>("Aircraft already exists.");
+            }
+
+            var success = await _db.Create(aircraft);
+
+            return Result.Ok<Aircraft>(success);
+        }
+        catch (Exception)
+        {
+            return Result.Exception<Aircraft>();
+        }
+    }
+
+    public async Task<Result<List<Aircraft>>> GetAllAircrafts()
+    {
+        try
+        {
+            var success = await _db.GetAll();
+
+            return Result.Ok<List<Aircraft>>(success);
+        }
+        catch (Exception)
+        {
+            return Result.Exception<List<Aircraft>>();
+        }
+    }
+
+}
---
+++ Domain/UseCases/ScheduleService.cs
@@ -0,0 +1,97 @@
+﻿using Domain.Logic;
+using Domain.IRepositories;
+using Domain.Models;
+
+namespace Domain.UseCases;
+public class ScheduleService
+{
+    private IScheduleRepository _db;
+
+    public ScheduleService(IScheduleRepository db)
+    {
+        _db = db;
+    }
+
+    public async Task<Result<Schedule>> GetSchedule(int id)
+    {
+        try
+        {
+            var success = await _db.Get(id);
+
+            if (success is null)
+            {
+                return Result.Fail<Schedule>("Schedule doesn't exist.");
+            }
+
+            return Result.Ok<Schedule>(success);
+        }
+        catch (Exception)
+        {
+            return Result.Exception<Schedule>();
+        }
+    }
+
+    public async Task<Result<Schedule>> DeleteSchedule(int id)
+    {
+        try
+        {
+            var success = await _db.Delete(id);
+
+            return Result.Ok<Schedule>(success);
+        }
+        catch (Exception)
+        {
+            return Result.Exception<Schedule>();
+        }
+    }
+
+    public async Task<Result<Schedule>> UpdateSchedule(Schedule schedule)
+    {
+        try
+        {
+            var success = await _db.Update(schedule);
+
+            return Result.Ok<Schedule>(success);
+        }
+        catch (Exception)
+        {
+            return Result.Exception<Schedule>();
+        }
+    }
+
+    public async Task<Result<Schedule>> CreateSchedule(Schedule schedule)
+    {
+        try
+        {
+            var item = await _db.Get(schedule.Id);
+
+            if (item is not null)
+            {
+                return Result.Fail<Schedule>("Schedule already exists.");
+            }
+
+            var success = await _db.Create(schedule);
+
+            return Result.Ok<Schedule>(success);
+        }
+        catch (Exception)
+        {
+            return Result.Exception<Schedule>();
+        }
+    }
+
+    public async Task<Result<List<Schedule>>> GetAllSchedules()
+    {
+        try
+        {
+            var success = await _db.GetAll();
+
+            return Result.Ok<List<Schedule>>(success);
+        }
+        catch (Exception)
+        {
+            return Result.Exception<List<Schedule>>();
+        }
+    }
+
+}

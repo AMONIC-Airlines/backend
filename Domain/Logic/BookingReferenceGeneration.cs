3daa1b9c6b2ae96b7639645bafd82118d9c7f959
MaximLebedevj <maximlebedevj@gmail.com>
2023-02-03 23:45:32 +0300

    added bookingReferenceGeneration

---
+++ Domain/Logic/BookingReferenceGeneration.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace Domain.Logic;
+public class BookingReferenceGeneration
+{
+    public static string GenerateBookingReference()
+    {
+        Random random = new Random();
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        return new string(Enumerable.Repeat(chars, 6)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+}
